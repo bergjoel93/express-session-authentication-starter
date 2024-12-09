3e94e3b18fd1f69921fc6bdf6e48051bbac643c7
Joel Berg <bergjoel93@gmail.com>
2024-12-09 12:45:16 -0700

    finished the tutorial, video 5

--- app.js
+++ app.js
@@ -1,44 +1,54 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
-var passport = require('passport');
-var crypto = require('crypto');
-var routes = require('./routes');
-const connection = require('./config/database');
+// app.js
 
-// Package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
-
-// Need to require the entire Passport config module so app.js knows about it
-require('./config/passport');
-
-/**
- * -------------- GENERAL SETUP ----------------
- */
-
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const crypto = require("crypto");
+const routes = require("./routes");
+const connection = require("./config/database");
 // Gives us access to variables set in the .env file via `process.env.VARIABLE_NAME` syntax
-require('dotenv').config();
+require("dotenv").config();
 
 // Create the Express application
 var app = express();
-
+/**
+ * -------------- GENERAL SETUP ----------------
+ */
+// Middleware to parse JSON and URL-encoded data
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-
+app.use(express.urlencoded({ extended: true }));
 /**
  * -------------- SESSION SETUP ----------------
  */
+// Library that stores session data in pgdb.
+const pgStore = require("connect-pg-simple")(session);
 
-// TODO
+const sessionStore = new pgStore({ pool: connection });
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    store: sessionStore,
+    cookie: { maxAge: 30 * 24 * 60 * 60 * 1000 }, // 30 days
+  })
+);
 
 /**
- * -------------- PASSPORT AUTHENTICATION ----------------
+ * -------------- Passoirt Authentication ----------------
  */
 
+// Need to require entire Passport config module so app.js knows about it.
+require("./config/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use((req, res, next) => {
+  console.log(req.session);
+  console.log(req.user);
+  next();
+});
 
 /**
  * -------------- ROUTES ----------------
@@ -47,10 +57,9 @@ app.use(passport.session());
 // Imports all of the routes from ./routes/index.js
 app.use(routes);
 
-
 /**
  * -------------- SERVER ----------------
  */
 
 // Server listens on http://localhost:3000
-app.listen(3000);
+app.listen(3000, () => console.log(`App listening on port 3000!`));
--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,54 @@
-const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
-const connection = require('./database');
-const User = connection.models.User;
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const pool = require("./database");
+const validatePassword = require("../lib/passwordUtils").validatePassword;
 
-// TODO: passport.use();
+const customFields = {
+  usernameField: "uname",
+  passwordField: "pw",
+};
+
+const verifyCallback = async (username, password, done) => {
+  try {
+    const { rows } = await pool.query(
+      "SELECT * FROM users WHERE username = $1",
+      [username]
+    );
+    const user = rows[0];
+    console.log("im in verify callback");
+    if (!user) {
+      return done(null, false, { message: "Incorrect username" });
+    }
+
+    const isValid = validatePassword(password, user.hash, user.salt);
+
+    if (isValid) {
+      return done(null, user);
+    } else {
+      return done(null, false);
+    }
+  } catch (error) {
+    return done(error);
+  }
+};
+
+const strategy = new LocalStrategy(customFields, verifyCallback);
+
+passport.use(strategy);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM users WHERE id = $1", [
+      id,
+    ]);
+    const user = rows[0];
+
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
